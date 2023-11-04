9f1fad0c0f57f25abaab986144ef32c5b6b905c9
Ilham Natsir <ilhamnatsirb@gmail.com>
2023-11-05 02:26:36 +0800

    menu listmusic dan menu download dari yutub

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask, render_template, send_from_directory, jsonify
+
+from flask import Flask, render_template, send_from_directory, request, redirect, url_for
+from pytube import YouTube
 
 app = Flask(__name__, static_folder='assets', template_folder='templates')
 
@@ -31,9 +33,41 @@ def index():
     return render_template('index.html', video_list=video_list)
 
 
+
+@app.route('/listmusic')
+def listmusik():
+    video_list = get_video_list()
+    return render_template('listmusik.html', video_list=video_list)
+
+
+# @app.route('/downloadmusic')
+# def downloadmusik():
+#     video_list = get_video_list()
+#     return render_template('download.html', video_list=video_list)
+
+
 @app.route('/video/<filename>')
 def video(filename):
     return send_from_directory('listvideo', filename)
+
+
+@app.route('/downloadmusic', methods=['GET', 'POST'])
+def download():
+    message = ""
+    if request.method == 'POST':
+        link = request.form['link']
+        try:
+            youtube_object = YouTube(link)
+            youtube_stream = youtube_object.streams.get_by_resolution("360p")
+
+            # Tentukan folder penyimpanan
+            output_path = 'listvideo/'
+            youtube_stream.download(output_path=output_path)
+
+            message = "Download is completed successfully"
+        except:
+            message = "An error has occurred"
+    return render_template('download.html', message=message)
 
 
 # @app.route('/video_list_json')
